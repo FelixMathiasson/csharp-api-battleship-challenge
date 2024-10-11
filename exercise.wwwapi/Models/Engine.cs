71283a54f231a3dc04c583b14cf197dec3d1877d
Felix <felix.mathiasson99@gmail.com>
2024-10-11 16:05:48 +0200

    game loop kind of coded

--- exercise.wwwapi/Models/Engine.cs
+++ exercise.wwwapi/Models/Engine.cs
@@ -2,5 +2,56 @@
 {
     public class Engine
     {
+        public string id { get; }
+        public Player player1 { get; }
+        public Player player2 {get;}
+        public Player current { get; set; }
+
+        public Engine(string firstPlayerName, string secondPlayerName, int mapSize)
+        {
+            id = Guid.NewGuid().ToString();
+            player1 = new Player(firstPlayerName, mapSize);
+            player2 = new Player(secondPlayerName, mapSize);
+            current = player1;
+        }
+         
+        public bool ShootTurn(Player player, int x, int y)
+        {
+            var enemy = player == player1 ? player2 : player1;
+
+            if(!enemy.selfMap.ValidCoordinate(x, y))
+            {
+                throw new Exception("ERROR! Invalid coordinates!");
+            }
+            if (!enemy.selfMap.tiles[x, y].hit)
+            {
+                enemy.selfMap.tiles[x, y].hit = true;
+                player.targetMap.tiles[x, y].hit = true;
+
+                if(enemy.selfMap.tiles[x, y].ship)
+                {
+                    foreach(var ship in enemy.ships)
+                    {
+                        if (ship.coordinates.Any(coord => coord.x == x && coord.y == y))
+                        {
+                            ship.coordinates.RemoveAll(coord => coord.x == x && coord.y == y);
+                            if(ship.coordinates.Count == 0)
+                            {
+                                ship.dead = true;
+                            }
+                            break;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("ERROR! Tile has already been shot!");
+            }
+
+            current = enemy;
+            return enemy.ships.All(s => s.dead);
+        }
     }
 }
--- exercise.wwwapi/Models/Player.cs
+++ exercise.wwwapi/Models/Player.cs
@@ -2,9 +2,19 @@
 {
     public class Player
     {
-        public string name {  get; set; }
+        public string name {  get; }
         public List<Ship> ships { get; set; }
-        public Map selfMap { get; set; }
-        public Map targetMap { get; set; }
+        public Map selfMap { get; }
+        public Map targetMap { get;}
+        string id { get; }
+
+        public Player(string name, int mapSize)
+        {
+            id = Guid.NewGuid().ToString();
+            this.name = name;
+            selfMap = new Map(mapSize);
+            targetMap = new Map(mapSize);
+            ships = new List<Ship>();
+        }
     }
 }
